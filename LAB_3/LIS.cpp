1d4673f602a3f771a0877cdce90e5c935f6271af
HienTran <hientm0978@gmail.com>
2024-10-19 23:51:07 +0700

    bai_1

--- LAB_3/LIS.cpp
+++ LAB_3/LIS.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 int LIS(vector<int> a, int n, vector<int> L)
 {
+	int maxElement = L[0];
 	//duyệt qua từng phần tử đứng trước chỉ số i
 	for (int i = 0; i < n; i++)
 	{
@@ -15,14 +16,8 @@ int LIS(vector<int> a, int n, vector<int> L)
 				L[i] = max(L[i], L[j] + 1);
 			}
 		}
-	}
-
-	//tìm phần tử lớn nhất là độ dài của chuỗi con tăng dài nhất trong mảng 
-	int maxElement = L[0];
-	for (int i = 0; i < L.size(); i++)
-	{
-		if (L[i] >= maxElement)
-			maxElement = L[i];
+		//Gán phần tử maxElement với giá trị cao nhất
+		maxElement = max(maxElement, L[i]);
 	}
 
 	//Trả về độ dài của chuỗi con tăng dài nhất
